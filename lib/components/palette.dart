064dc39f709fdfc1834f5e77cda61d1de2fff087
lopo <lopo@zju.edu.cn>
2025-10-13 15:05:08 +0800

    feat: custom palette

---
+++ lib/components/palette.dart
@@ -0,0 +1,70 @@
+import 'package:bookkeeping/constants/color.dart';
+import 'package:flutter/cupertino.dart';
+
+class BKPPalette extends StatelessWidget {
+  static const _activeDecoration = ShapeDecoration(
+    shape: CircleBorder(
+      side: BorderSide(width: 2, color: CupertinoColors.systemGrey4),
+    ),
+  );
+  static const _inactiveDecoration = ShapeDecoration(
+    shape: CircleBorder(
+      side: BorderSide(width: 2, color: CupertinoColors.transparent),
+    ),
+  );
+
+  final BKPColor color;
+  final ValueChanged<BKPColor> onTap;
+
+  void _onTap(BKPColor v) {
+    if (v == color) return;
+    onTap(v);
+  }
+
+  const BKPPalette({super.key, required this.color, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    CupertinoColors.destructiveRed;
+    return Wrap(
+      alignment: WrapAlignment.start,
+      children: [
+        for (final item in BKPColor.all)
+          GestureDetector(
+            onTap: () => _onTap(item),
+            child: Container(
+              width: 36,
+              height: 36,
+              margin: EdgeInsets.all(4),
+              alignment: Alignment.center,
+              decoration:
+                  item == color ? _activeDecoration : _inactiveDecoration,
+              child: Icon(
+                CupertinoIcons.circle_fill,
+                size: 32,
+                color: item.color,
+              ),
+            ),
+          ),
+        // SizedBox(
+        //   width: 44,
+        //   height: 44,
+        //   child: Center(
+        //     child: DecoratedBox(
+        //       decoration:
+        //           item == color ? _activeDecoration : _inactiveDecoration,
+        //       child: Padding(
+        //         padding: EdgeInsets.all(2),
+        //         child: Icon(
+        //           CupertinoIcons.circle_fill,
+        //           size: 36,
+        //           color: item.color,
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
---
+++ lib/constants/color.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+
+class BKPColor {
+  static const List<BKPColor> all = [
+    BKPColor.blue(),
+    BKPColor.green(),
+    BKPColor.mint(),
+    BKPColor.indigo(),
+    BKPColor.orange(),
+    BKPColor.pink(),
+    BKPColor.brown(),
+    BKPColor.purple(),
+    BKPColor.red(),
+    BKPColor.teal(),
+    BKPColor.cyan(),
+    BKPColor.yellow(),
+    BKPColor.grey(),
+  ];
+
+  static BKPColor findByName(String name) {
+    for (final item in all) {
+      if (item.name == name) return item;
+    }
+    return all[0];
+  }
+
+  final String name;
+  final Color color;
+
+  const BKPColor._(this.name, this.color);
+
+  const BKPColor.blue()
+      : name = 'Blue',
+        color = CupertinoColors.systemBlue;
+
+  const BKPColor.green()
+      : name = 'Green',
+        color = CupertinoColors.systemGreen;
+
+  const BKPColor.mint()
+      : name = 'Mint',
+        color = CupertinoColors.systemMint;
+
+  const BKPColor.indigo()
+      : name = 'Indigo',
+        color = CupertinoColors.systemIndigo;
+
+  const BKPColor.orange()
+      : name = 'Orange',
+        color = CupertinoColors.systemOrange;
+
+  const BKPColor.pink()
+      : name = 'Pink',
+        color = CupertinoColors.systemPink;
+
+  const BKPColor.brown()
+      : name = 'Brown',
+        color = CupertinoColors.systemBrown;
+
+  const BKPColor.purple()
+      : name = 'Purple',
+        color = CupertinoColors.systemPurple;
+
+  const BKPColor.red()
+      : name = 'Red',
+        color = CupertinoColors.systemRed;
+
+  const BKPColor.teal()
+      : name = 'Teal',
+        color = CupertinoColors.systemTeal;
+
+  const BKPColor.cyan()
+      : name = 'Cyan',
+        color = CupertinoColors.systemCyan;
+
+  const BKPColor.yellow()
+      : name = 'Yellow',
+        color = CupertinoColors.systemYellow;
+
+  const BKPColor.grey()
+      : name = 'Grey',
+        color = CupertinoColors.systemGrey;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is BKPColor && name == other.name && color == other.color;
+  }
+
+  @override
+  int get hashCode => Object.hash(name, color);
+}

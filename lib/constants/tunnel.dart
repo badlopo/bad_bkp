b168c35b629446f6edb6cf8dd4e2e3b217039acb
lopo <lopo@zju.edu.cn>
2025-11-06 00:21:33 +0800

    feat: BKPTunnel

--- lib/constants/tunnel.dart
+++ lib/constants/tunnel.dart
@@ -1,9 +1,43 @@
-abstract class BKPTunnels {
-  /// events:
-  /// - `#create`
-  static const category = #category;
+import 'package:bookkeeping/utils/tunnel.dart';
 
-  /// events:
-  /// - `#create`
-  static const tag = #tag;
+enum BKPTunnelName {
+  /// custom tab 下的事件
+  ///
+  /// - `#category` 新建 category
+  /// - `#tag` 新建 tag
+  custom,
+
+  /// 刷新列表
+  ///
+  /// - `#category`
+  /// - `#tag`
+  /// - `#transaction`
+  refresh,
+}
+
+abstract class BKPTunnel {
+  /// `BKPTunnelName.custom` 事件
+  ///
+  /// ---
+  ///
+  /// [to]
+  /// - `#category`
+  /// - `#tag`
+  static void sendCustom(Symbol to) {
+    assert(const {#category, #tag}.contains(to));
+    Tunnel(BKPTunnelName.custom).send(to);
+  }
+
+  /// `BKPTunnelName.refresh` 事件
+  ///
+  /// ---
+  ///
+  /// [to]
+  /// - `#category`
+  /// - `#tag`
+  /// - `#transaction`
+  static void sendRefresh(Symbol to) {
+    assert(const {#category, #tag, #transaction}.contains(to));
+    Tunnel(BKPTunnelName.refresh).send(to);
+  }
 }
